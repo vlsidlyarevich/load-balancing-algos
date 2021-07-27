101a256e750cdf6629ff05d3b11047421c3d93fc
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2021-07-27 22:23:36 +0300

    echo-server: http hello world added

--- echo-server/cmd/echo-server/main.go
+++ echo-server/cmd/echo-server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "log"
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	log.Println("Hello world!")
+
+	var handler = func(w http.ResponseWriter, r *http.Request) {
+		_, err := io.WriteString(w, "Hello, world!\n")
+		if err != nil {
+			log.Println("Error during handling response: ", err)
+		}
+	}
+
+	http.HandleFunc("/hello", handler)
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
