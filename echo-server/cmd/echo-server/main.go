2a7ce5996da1bcf265ad844bc52c6dd8e5b564be
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2021-07-27 23:15:51 +0300

    echo-server: refactoring

--- echo-server/cmd/echo-server/main.go
+++ echo-server/cmd/echo-server/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	log.Println("Hello world!")
+	var server = NewServer(":8081")
+	log.Fatal(server.Start())
+}
 
-	var handler = func(w http.ResponseWriter, r *http.Request) {
-		_, err := io.WriteString(w, "Hello, world!\n")
-		if err != nil {
-			log.Println("Error during handling response: ", err)
-		}
+type Server struct {
+	Port   string
+	Router *mux.Router
+}
+
+func NewServer(port string) *Server {
+	s := Server{port, mux.NewRouter()}
+	configureRoutes(&s)
+	return &s
+}
+
+func (s *Server) Start() error {
+	log.Println("Starting server on port", s.Port)
+	return http.ListenAndServe(s.Port, s.Router)
+}
+
+func configureRoutes(s *Server) {
+	s.Router.HandleFunc("/hello", HelloHandler).Methods("GET")
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := io.WriteString(w, "Hello, world!\n")
+	if err != nil {
+		log.Println("Error during handling response: ", err)
 	}
-
-	http.HandleFunc("/hello", handler)
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
 }
